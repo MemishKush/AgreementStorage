9defa26a81d738ac4244ff04600d64c8d51662c2
MemishKush <MemishKush@users.noreply.github.com>
2020-03-01 14:37:06 +0200

    Config

--- src/main/java/Agreement.java
+++ src/main/java/Agreement.java
@@ -9,6 +9,12 @@ public class Agreement {
     public Agreement() {
     }
 
+    public Agreement(String name, String signedBy, List<Product> productList) {
+        this.name = name;
+        this.signedBy = signedBy;
+        this.productList = productList;
+    }
+
     public String getName() {
         return name;
     }
@@ -31,5 +37,17 @@ public class Agreement {
 
     public void setProductList(List<Product> productList) {
         this.productList = productList;
+    }
+    Agreement agreement = new Agreement("01/03/202","Peter",getProductList());
+    public String toString(Object a){
+         a = agreement;
+
+
+        return "name: " + agreement.getName() + "signed By: " + agreement.getSignedBy() + "Products: " + agreement.getProductList();
+    }
+    public void print(){
+
+
+
     }
 }
